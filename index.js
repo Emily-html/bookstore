40e2383b85c6e85d69c205086aa7e6fd58324354
Emily-html <19195060852m@sina.cn>
2024-12-27 08:15:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const urlp=new URLSearchParams(window.location.search);
 const olink=urlp.get('olink');
 function f1(){
-    document.getElementById("lin").href=olink;
+  if(olink!=null)  document.getElementById("lin").href=olink;
 }
 function form(){
     document.getElementById('form1').style.display='block';
